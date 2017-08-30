f897c4829f4416868e3459a35ab93df2bb1fe4c5
ashardweedar <ashar.dweedar@gmail.com>
2017-08-30 20:26:10 +0300

    finish html, init, send, stop untill GET

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,30 @@
 // YOUR CODE HERE:
+var app ={
+	init: function(){
+		var allName = window.location.search;
+	 	var name = allName.slice(allName.search('=')+1).split('%20').join(' ')
+		var message = {
+			username: name,
+			text: $("#message").val(),
+			roomname: $("#room").val()
+		};
+		this.send(message);
+	},
+	send : function(message){
+		$.ajax({
+		  url:'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+		  type: 'POST',
+		  data : message,
+		  contentType: 'application/json',
+		  success: function (data) {
+		    console.log('chatterbox: Message sent');
+		  },
+		  error: function (data) {
+		    console.error('chatterbox: Failed to send message', data);
+		  }
+		});
+	},
+	fetch:function(){
+
+	}
+}
